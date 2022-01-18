7280bf58457735976afdf02cfcb88f6ab4c75f60
hyuk <dlsgur0906@gmail.com>
2022-01-18 11:25:27 +0900

    서버 요청하는거 테스트 해봤음. firstcommit한거 포함

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = 8080;
 app.use(express.json()); //json형식의 데이터를 처리할 수 있도록 설정
 app.use(cors());        // 브라우저의 다양한 사용을 위해 설정
 
-// get요청이 오면 res.send를 보내주겠다.
+// get요청이 오면 res.send를 보내주겠다. -> 브라우저에 http://localhost:8080/customers 입력해서 확인해보기
 app.get('/customers', async(req,res)=>{
     res.send('고객리스트 입니다.')
 })
-// post요청 오면 res.send를 보내주겠다.
+// post요청 오면 res.send를 보내주겠다. -> 포스트맨으로 http://localhost:8080/createcustomer확인해보기
 app.post('/createcustomer', async(req,res)=>{
     res.send('등록되었습니다.')
 })
