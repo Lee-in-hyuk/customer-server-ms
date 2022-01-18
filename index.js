cdce20266b702cf7bc34ec8b8e6685419616d8cf
hyuk <dlsgur0906@gmail.com>
2022-01-18 16:23:01 +0900

    database.json에 작성한거 index에 연결시키고 get요청 되는지 확인 완료

--- index.js
+++ index.js
@@ -5,39 +5,32 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
+const fs = require('fs');
+const dataj = fs.readFileSync("./database.json");
+const parseData = JSON.parse(dataj);    // database.json받아온거를 객체형태로 변수에 담음
+const mysql = require('mysql');
+
+// php에서 connect 만든거랑 같은 개념
+const connection = mysql.createConnection({
+    host:parseData.host,
+    user: parseData.user,
+    password: parseData.password,
+    port: parseData.port,
+    database: parseData.database
+})
+
 app.use(express.json()); //json형식의 데이터를 처리할 수 있도록 설정
 app.use(cors());        // 브라우저의 다양한 사용을 위해 설정
 
 // get요청이 오면 res.send를 보내주겠다. -> 브라우저에 http://localhost:8080/customers 입력해서 확인해보기
 app.get('/customers', async(req,res)=>{
-    res.send({
-        data :[
-            {
-              no:0,
-              name: "김그린",
-              phone: '010-1234-1234',
-              birthday: '1987-12-12',
-              gender: '남성',
-              addr: '울산 남구 삼산동 화합로 12'
-            },
-            {
-              no:1,
-              name: "이블루",
-              phone: '010-1234-4321',
-              birthday: '1987-12-13',
-              gender: '여성',
-              addr: '울산 남구 삼산동 화합로 15'
-            },
-            {
-              no:3,
-              name: "성레드",
-              phone: '010-4321-1234',
-              birthday: '1987-11-12',
-              gender: '여성',
-              addr: '울산 남구 삼산동 화합로 22'
-            },
-          ]
-    })
+    connection.query(
+        "SELECT * FROM customers",
+        (err, rows, fields) =>{
+            res.send(rows);
+        }
+    )
+    // 여기까지 작성하고 http://localhost:8080/customers 브라우저에 입력했을 때 데이터 잘 나오면 성공
 })
 // post요청 오면 res.send를 보내주겠다. -> 포스트맨으로 http://localhost:8080/createcustomer확인해보기
 app.post('/createcustomer', async(req,res)=>{
